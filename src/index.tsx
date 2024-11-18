fffe35cf1df69b09d0c033744acf29903df6014a
Palash A <palash.agarwal.96@gmail.com>
2024-11-18 12:45:26 -0500

    add packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import './globals.css';
 import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
